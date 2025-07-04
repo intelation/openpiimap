ef5030ec7ba6e7d4fb176873f4207229dd037714
intelation <mandaliya@gmail.com>
2025-07-04 18:47:41 +0100

    UI has been started.

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to OpenPIIMap Visualization</h1>
+      <p>Your interactive privacy compliance atlas.</p>
+    </div>
+  );
+};
+
+export default Home;
