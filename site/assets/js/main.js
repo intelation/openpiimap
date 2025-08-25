691a0a0ce430d946680b4cc44241d8b449dc2247
Kartik Kale <kartik.kale@Intelation.onmicrosoft.com>
2025-08-25 16:45:03 +0530

    Added Malaysia and Philippines with updated data files.

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -38,7 +38,9 @@ const countries = [
     { name: "Switzerland", framework: "FADP", region: "Europe", file: "fadp-switzerland.html", categories: 10 },
     { name: "South Africa", framework: "POPIA", region: "Africa", file: "popia-south-africa.html", categories: 12 },
 { name: "New Zealand", framework: "Privacy Act", region: "Asia Pacific", file: "privacyact-new-zealand.html", categories: 8 },
-{ name: "Bahamas", framework: "Privacy Act", region: "North America", file: "privacyact-bahamas.html", categories: 5 }
+{ name: "Bahamas", framework: "Privacy Act", region: "North America", file: "privacyact-bahamas.html", categories: 5 },
+{ name: "Malaysia", framework: "PDPA", region: "Asia", file: "pdpa-malaysia.html", categories: 10 },
+{ name: "Philippines", framework: "DPA", region: "Asia Pacific", file: "dpa-philippines.html", categories: 10 }
 ];
 
 // Framework color mapping
